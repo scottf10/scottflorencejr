ffbf78996facaf6d38a2fdbb131e0855c0139b10
scottf10 <61720721+scottf10@users.noreply.github.com>
2023-05-20 01:48:36 -0400

    Live slug

--- includes/config.php
+++ includes/config.php
@@ -3,6 +3,6 @@ session_start();
 include('admin/config/dbcon.php');
 
 function base_url($slug){
-    echo 'http://localhost/scottflo/php-blog/'.$slug;
+    echo 'http://scottflorencejr.com/'.$slug;
 }
 ?>
