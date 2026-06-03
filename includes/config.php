2186dc070b90a718a943c76a878cdfca8a360a42
scottf10 <61720721+scottf10@users.noreply.github.com>
2026-06-03 19:54:30 -0400

    Delete includes directory

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include('admin/config/dbcon.php');
-
-function base_url($slug){
-    echo 'http://scottflorencejr.com/'.$slug;
-    //echo 'http://localhost/scottflo/php-blog/'.$slug;
-}
-?>
